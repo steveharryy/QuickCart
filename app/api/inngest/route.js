07ce4d53773aaeddcd4f0f2de517117b7b6c1e9b
steveharryy <adinfotech22@gmail.com>
2025-12-01 19:42:07 +0530

    Initial upload

---
+++ app/api/inngest/route.js
@@ -0,0 +1,18 @@
+import { serve } from "inngest/next";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserUpdation,
+  syncUserDeletion
+} from "../../../config/inngest.js";
+
+
+export const { GET, POST } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
+  ],
+});
+
