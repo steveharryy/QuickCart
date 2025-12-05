6c126bee148bdac11676785ce16e3adc39dff0b7
Abhay Kumar <161684940+steveharryy@users.noreply.github.com>
2025-12-05 14:36:58 +0530

    Refactor ESLint configuration for Next.js and JS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import next from "eslint-plugin-next";
+import js from "@eslint/js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  js.configs.recommended,
+  next.configs["core-web-vitals"],
+  {
+    rules: {
+      // You can customize eslint rules here if needed
+    },
+  },
+];
